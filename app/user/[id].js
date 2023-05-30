2ba895de3f8750559ac88bc1ad0bd8bf1e6fbd03
Shawn Law <shawnlaw@tuta.io>
2023-05-30 17:49:00 +0800

    1.09.53 navigate to user profile

--- app/user/[id].js
+++ app/user/[id].js
@@ -1,15 +1,21 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { useRouter } from 'expo-router'
+import { useRouter, useSearchParams } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import users from '../../assets/data/users';
 
 const ProfilePage = () => {
 
   const router = useRouter();
+  const { id } = useSearchParams();
+
+  const user = users.find(u => u.id === id)
+  if(!user) <Text>User not found</Text>
 
   return (
     <SafeAreaView>
-      <Text>ProfilePage</Text>
+      <Text>Profile Page: {user.id}</Text>
+      <Text>Name: {user.name}</Text>
       <Text onPress={() => router.back()}>Go back</Text>
     </SafeAreaView>
   )
--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -10,7 +10,7 @@ const UserCard = ({user}) => {
   '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
   return (
-    <Link href={'/user'} asChild>
+    <Link href={`/user/${user.id}`} asChild>
       <Pressable>
         <ImageBackground
           style={styles.userCard} 
