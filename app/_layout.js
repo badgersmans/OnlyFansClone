0a41b76cd6b87d48da5adce650f1f164515bbe8c
Shawn Law <shawnlaw@tuta.io>
2023-06-01 11:40:50 +0800

    35.07 done authentication

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,15 @@
 import { Stack } from "expo-router";
 import { Amplify } from 'aws-amplify';
 import awsExports from '../src/aws-exports'
+import { Authenticator } from '@aws-amplify/ui-react-native';
 Amplify.configure(awsExports);
 
 export default function RootLayout() {
-    return <Stack screenOptions={{ headerShown: false }}/>
+    return (
+        <Authenticator.Provider>
+            <Authenticator>
+                <Stack screenOptions={{ headerShown: false }}/>
+            </Authenticator>
+        </Authenticator.Provider>
+    )
 };
--- app/index.js
+++ app/index.js
@@ -1,17 +1,20 @@
+import { Text } from "react-native";
 import { FlashList } from "@shopify/flash-list";
 import users from "../assets/data/users";
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import styles from "../Index.Styles";
 import UserCard from "../src/components/UserCard/UserCard";
 import { Link } from "expo-router";
+import { useAuthenticator } from '@aws-amplify/ui-react-native';
 
 export default function Page() {
-  // const user = users[0]
+  const { signOut } = useAuthenticator()
 
   return (
     <SafeAreaProvider style={styles.container}>
       <SafeAreaView style={{flex: 1}}>
         <Link href={'/newPost'}>New Post</Link>
+        <Text onPress={signOut}>Sign Out</Text>
         <FlashList 
           data={users}
           renderItem={({ item }) => <UserCard user={item} /> }
